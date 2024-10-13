31f0df9b0df070545f4e12dfdf4494dfec3b3ae3
adriano-gil <adrianoagiljr@gmail.com>
2024-10-13 18:06:07 -0300

    adicionando arquivos

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photoCover": "https://i.ytimg.com/vi/HwbHiulNG_4/maxresdefault.jpg",
+        "title": "Lançamento de Batman: A Nova Era",
+        "description": "A DC Comics anunciou o lançamento de uma nova série em quadrinhos chamada Batman: A Nova Era, que explora a relação do Cavaleiro das Trevas com os novos vigilantes de Gotham. A série promete trazer um enredo envolvente, com vilões clássicos e novas ameaças, além de uma arte impressionante. A estreia está marcada para janeiro de 2025."
+    },
+    {
+        "id": "2",
+        "photoCover": "https://observatoriodocinema.uol.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=1200&height=675&format=.jpg&quality=91&imagick=uploads-observatoriodocinema.seox.com.br/2024/05/superman-david-corenswet.jpg",
+        "title": "Rumores sobre o Novo Filme do Superman",
+        "description": "Fontes próximas à produção do novo filme do Superman indicam que o diretor James Gunn está em negociações para trazer uma nova abordagem ao icônico personagem. O filme, que se chamará Superman: Renascimento, pretende focar na juventude de Clark Kent e sua jornada para se tornar o herói que todos conhecem. Expectativa alta entre os fãs para um anúncio oficial!"
+
+    },
+    {
+        "id": "3",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC05wVq8E7AAjYqKhMbuBlzq1NdeKdoqE85Q&s",
+        "title": "Os Novos Guardiões em Desenvolvimento",
+        "description": "A HBO Max confirmou que está desenvolvendo uma nova série chamada Liga da Justiça: Os Novos Guardiões. A série acompanhará a nova geração de super-heróis, incluindo personagens como Jon Kent (Superman), Yara Flor (Wonder Girl) e outros. A produção promete trazer questões contemporâneas e diversidade ao universo DC."
+
+    },
+    {
+        "id": "4",
+        "photoCover": "https://geeksaw.com.br/wp-content/uploads/2020/06/harley-quinn-t-herapist-make-em-laugh-1223109.jpeg",
+        "title": "A Volta de Harley Quinn nos Quadrinhos",
+        "description": "Harley Quinn está de volta com uma nova série de quadrinhos que se concentra em suas aventuras solo. Intitulada Harley Quinn: Liberdade de Loucura, a série explora o lado mais complexo da personagem, equilibrando comédia e drama. A escritora Amanda Conner retorna para capturar a essência única de Harley, e os fãs já estão animados para ver o que a Arlequina fará a seguir."
+
+    },
+
+];
